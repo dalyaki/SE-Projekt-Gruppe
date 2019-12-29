e1e33c16d777fd9a0b71db974d998c1886bd6688
dalyaki <46282572+dalyaki@users.noreply.github.com>
2019-12-29 22:59:16 +0200

    Create MyTextField.java
    JTEXTFIELD WITH  CHARACTER LIMIT

---
+++ src/MyTextField.java
@@ -0,0 +1,32 @@
+import javax.swing.text.*;
+import javax.swing.*;
+
+class FixedSizeDocument extends PlainDocument{
+	private int max = 256;
+	public FixedSizeDocument(int max){
+		this.max = max;
+		}
+	@Override
+	public void insertString(int offs, String str, AttributeSet a)throws BadLocationException{
+		if (getLength()+str.length()>max){
+			str = str.substring(0, max - getLength());
+		}
+
+		super.insertString(offs, str, a);
+
+	}
+
+}
+
+public class MyTextField extends JTextField{
+
+		MyTextField(){
+      super();
+      setDocument(new FixedSizeDocument(256));
+    }
+    MyTextField(int x){
+      super(x);
+      setDocument(new FixedSizeDocument(256));
+    }
+
+}
